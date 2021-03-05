f655649d7bafc0f6464bf15964fbd86fbeaaf0a0
marchuanv <marchuanv@gmail.com>
2021-03-05 14:40:04 +0200

    refactor

--- component.request.handler.unsecure.js
+++ component.request.handler.unsecure.js
@@ -1,10 +1,7 @@
 const requestHandlerUser = require("component.request.handler.user");
-const utils = require("utils");
 const delegate = require("component.delegate");
-const logging = require("logging");
-logging.config.add("Request Handler Unsecure");
 
-module.exports = { 
+module.exports = {
     handle: (context, options) => {
         const name = `${options.port}${options.path}`;
         requestHandlerUser.handle(context, options);
