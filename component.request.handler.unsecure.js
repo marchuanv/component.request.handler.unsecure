570f7ff8df184ea1f4944f3d831688f8cdb3cb89
marchuanv <marchuanv@gmail.com>
2021-03-23 09:48:29 +0200

    refactor

--- component.request.handler.unsecure.js
+++ component.request.handler.unsecure.js
@@ -1,12 +1,13 @@
-const requestHandlerUser = require("component.request.handler.user");
-const delegate = require("component.delegate");
-
-module.exports = {
-    handle: (context, options) => {
-        const name = `${options.port}${options.path}`;
-        requestHandlerUser.handle(context, options);
-        delegate.register("component.request.handler.unsecure", name, async ({ session,  data }) => {
-            return await delegate.call({ context, name }, { session, data });
-        });
-    }
-};
+const component = require("component");
+component.register({ moduleName: "component.request.handler.unsecure" }).then( async ({ requestHandlerUnsecure }) => {
+    const { config } = await component.load({ moduleName: "component.request.handler.route" });
+    const { routes, port } = config.requestHandlerRoute;
+    for(const route of routes){
+        if (route.secure === false){
+            const name = `${port}${route.path}`;
+            requestHandlerUnsecure.subscribe( { name }, async ({ session, data }) => {
+                return await requestHandlerUnsecure.publish({ name }, { session, data });
+            });
+        }
+    };
+});
