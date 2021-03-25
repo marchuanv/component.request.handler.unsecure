7fd87d766b2a93a21c39d62a2cd99ceb0465a495
marchuanv <marchuanv@gmail.com>
2021-03-25 03:18:08 +0200

    refactor

--- component.request.handler.unsecure.js
+++ component.request.handler.unsecure.js
@@ -1,7 +1,7 @@
 const component = require("component");
-component.register({ moduleName: "component.request.handler.unsecure" }).then( async ({ requestHandlerUnsecure }) => {
-    const { config } = await component.load({ moduleName: "component.request.handler.route" });
-    const { routes, port } = config.requestHandlerRoute;
+component.register(module).then( async ({ requestHandlerUnsecure }) => {
+    const { requestHandlerRoute } = await component.register("component.request.handler.route");
+    const { routes, port } = requestHandlerRoute;
     for(const route of routes){
         if (route.secure === false){
             const name = `${port}${route.path}`;
