449a651fcd3ad9c79669dc8711db2e652da83c81
marchuanv <marchuanv@gmail.com>
2021-03-30 03:15:48 +0200

    refactor

--- component.request.handler.unsecure.js
+++ component.request.handler.unsecure.js
@@ -1,13 +1,7 @@
 const component = require("component");
 component.register(module).then( async ({ requestHandlerUnsecure }) => {
-    const { requestHandlerRoute } = await component.register("component.request.handler.route");
-    const { routes, port } = requestHandlerRoute;
-    for(const route of routes){
-        if (route.secure === false){
-            const name = `${port}${route.path}`;
-            requestHandlerUnsecure.subscribe( { name }, async ({ session, data }) => {
-                return await requestHandlerUnsecure.publish({ name }, { session, data });
-            });
-        }
-    };
+    const { channel } = requestHandlerUnsecure;
+    requestHandlerUnsecure.subscribe( { channel }, async ({ session, data }) => {
+        return await requestHandlerUnsecure.publish({ channel }, { session, data });
+    });
 });
