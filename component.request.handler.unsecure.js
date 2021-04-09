0cdbb1c379d3331437807fda4ee298064e655709
marchuanv <marchuanv@gmail.com>
2021-04-10 00:43:14 +0200

    refactor

--- component.request.handler.unsecure.js
+++ component.request.handler.unsecure.js
@@ -1,7 +1,6 @@
 const component = require("component");
-component.register(module).then( async ({ requestHandlerUnsecure }) => {
-    const { channel } = requestHandlerUnsecure;
-    requestHandlerUnsecure.subscribe( { channel }, async ({ session, data }) => {
-        return await requestHandlerUnsecure.publish({ channel }, { session, data });
+component.load(module).then(async ({ requestHandlerUnsecure }) => {
+    requestHandlerUnsecure.subscribe(async ({ session, data }) => {
+        return await requestHandlerUnsecure.publish({ session, data });
     });
 });
